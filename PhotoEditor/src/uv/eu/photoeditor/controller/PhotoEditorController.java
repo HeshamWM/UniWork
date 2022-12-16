6bd770799beaabc0a3073e731cf79fe58866b704
Hesham Mostafa <heshamwael.hw@gmail.com>
2022-12-16 16:18:29 +0100

    Drawing on picture feature added + gradient that is not customisable yet

--- PhotoEditor/src/uv/eu/photoeditor/controller/PhotoEditorController.java
+++ PhotoEditor/src/uv/eu/photoeditor/controller/PhotoEditorController.java
@@ -4,12 +4,21 @@
  */
 package uv.eu.photoeditor.controller;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 import uv.eu.photoeditor.model.PhotoEditorModel;
+import uv.eu.photoeditor.view.LoadImage;
 import uv.eu.photoeditor.view.PhotoEditorView;
 
 /**
@@ -19,6 +28,19 @@ import uv.eu.photoeditor.view.PhotoEditorView;
 public class PhotoEditorController {
     private PhotoEditorModel model;
     private PhotoEditorView view;
+    private LoadImage loadImage;
+    private Color selectedColor1;
+    private Color selectedColor2;
+    int ID;
+    
+    public Color getSelectedColor(int ID){
+        if (ID == 1){
+            return selectedColor1;
+        }else{
+            return selectedColor2;
+        }
+    }
+    
     
     public PhotoEditorController (PhotoEditorModel model, PhotoEditorView view){
         this.model = model;
@@ -42,36 +64,88 @@ public class PhotoEditorController {
         @Override
         public void actionPerformed(ActionEvent ae) {
             String command = ae.getActionCommand();
-            switch (command) {
-                case "salir":
-                    System.out.println("EuroConversorController : Boton ’salir ’. ");
-                    System.exit(0);
-                    break;
-                case "0":
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "C":
-                case ".":
-                    break;
-                case "change rate": 
-                    break;
-                case "clear":
-                               
-                    break;
-                case "convert":
-             
-                    break;
-                default:
-                    System.out.println(" EuroConversorController : Comando ’" + command + "’ no reconocido.");
-                    break;
+            Character firstChar = command.charAt(0);
+            if (firstChar.equals('1') || firstChar.equals('2')){
+                ID = Integer.parseInt(Character.toString(command.charAt(0)));
+                command = command.substring(1);
+                if (ID == 1){
+                    selectedColor1 = view.select.panel1.getColor(command);
+                    view.status.setColorStatus(selectedColor1, ID);
+                } 
+                else if (ID == 2){
+                    selectedColor2 = view.select.panel2.getColor(command);
+                    view.status.setColorStatus(selectedColor2, ID);
+                }
+            }
+            else
+            {
+                switch (command) {
+                    case "Salir":
+                        System.out.println("Boton ’salir ’. ");
+                        System.exit(0);
+                        break;
+                    case "Cargar Imagen":
+                        loadImage = new LoadImage();
+                        model.loadImagen(loadImage.getFile());
+                        view.repaintImage();
+                        break;
+                    case "change rate": 
+                        break;
+                    case "clear":
+                        break;
+                    case "convert":
+                        break;
+                    default:
+                        System.out.println("Comando ’" + command + "’ no reconocido.");
+                        break;
+                }
             }
         }
+    }
+    public class PhotoEditorMouseListener implements MouseListener{
+        private ArrayList<Integer> x = new ArrayList<>();;
+        private ArrayList<Integer> y = new ArrayList<>();;
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            Graphics g = view.image.getGraphics();
+            Graphics2D g2d = (Graphics2D)g;
+
+            if (SwingUtilities.isRightMouseButton(e)){
+                int nPoints = x.size();
+                int[] xArray = new int[x.size()];
+                int[] yArray = new int[y.size()];
+
+                for (int i=0;i<x.size();i++){
+                    xArray[i] = x.get(i);
+                    yArray[i] = y.get(i);
+                }
+                /*
+                Color color1 = Color.RED;
+                Color color2 = Color.GREEN;
+                GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
+                g2d.setPaint(gp);
+                */
+                g2d.setStroke(new BasicStroke(20.0f));
+                g.drawPolygon(xArray, yArray, nPoints);
+                x.clear();
+                y.clear();
+            }
+            else
+            {
+                x.add(e.getX());
+                y.add(e.getY());
+                g.fillOval(e.getX()-10, e.getY()-10, 20, 20);
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {}
+        @Override
+        public void mouseReleased(MouseEvent e) {}
+        @Override
+        public void mouseEntered(MouseEvent e) {}
+        @Override
+        public void mouseExited(MouseEvent e) {}
     }
 }
